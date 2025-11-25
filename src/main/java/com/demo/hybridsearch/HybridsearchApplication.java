0da5dd496668cd57cb759fcc782cbfbb02bab824
박건우 <parkwoo9901@gmail.com>
2025-11-25 10:03:33 +0900

    Initial Commit

---
+++ src/main/java/com/demo/hybridsearch/HybridsearchApplication.java
@@ -0,0 +1,13 @@
+package com.demo.hybridsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HybridsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HybridsearchApplication.class, args);
+    }
+
+}
