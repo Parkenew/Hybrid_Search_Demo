45d0567d9e03cb90dacb97360b94a4ec79e3c424
박건우 <parkwoo9901@gmail.com>
2025-11-27 17:24:07 +0900

    refactor: app.jsx 는 라우팅만 담당하도록 페이지 분리

--- hybridsearch_fe/src/App.jsx
+++ hybridsearch_fe/src/App.jsx
@@ -1,22 +1,14 @@
-import ExplainSection from "./components/ExplainSection";
-import SearchBar from "./components/SearchBar";
-import LogoSection from "./components/LogoSection";
-import FooterSection from "./components/FooterSection";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SearchResultPage from "./pages/SearchResultPage";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-white flex flex-col">
-      {/* 🔥 가운데 카드 섹션 (항상 세로 중앙 유지) */}
-      <div className="flex-1 flex flex-col justify-center items-center">
-        <div className="w-full max-w-[750px] flex flex-col gap-5">
-          <LogoSection />
-          <SearchBar />
-          <ExplainSection />
-        </div>
-      </div>
-
-      {/* 🔥 푸터 섹션 (항상 맨 아래, 중앙 섹션 깨지지 않음) */}
-      <FooterSection />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/result" element={<SearchResultPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
---
+++ hybridsearch_fe/src/pages/HomePage.jsx
@@ -0,0 +1,26 @@
+import ExplainSection from "../components/ExplainSection";
+import SearchBar from "../components/SearchBar";
+import LogoSection from "../components/LogoSection";
+import FooterSection from "../components/FooterSection";
+import ThemeToggle from "../components/ThemeToggle";
+
+export default function HomePage() {
+  return (
+    <div className="min-h-screen bg-white flex flex-col dark:bg-gray-700 dark:text-white transition-colors">
+      {/* 🔥 가운데 카드 섹션 (항상 세로 중앙 유지) */}
+      <div className="absolute top-4 right-4">
+        <ThemeToggle />
+      </div>
+      <div className="flex-1 flex flex-col justify-center items-center">
+        <div className="w-full max-w-[750px] flex flex-col gap-5">
+          <LogoSection />
+          <SearchBar />
+          <ExplainSection />
+        </div>
+      </div>
+
+      {/* 🔥 푸터 섹션 (항상 맨 아래, 중앙 섹션 깨지지 않음) */}
+      <FooterSection />
+    </div>
+  );
+}
