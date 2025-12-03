a2ca6729e171b17d4eb414743827aefa2015e33d
박건우 <parkwoo9901@gmail.com>
2025-12-03 17:03:41 +0900

    feat: 검색 로직 추가
    - 키워드, 벡터 검색 로직
    - parse()함수: ES 검색 결과(JSON) 파싱 및 반환 DTO 생성

--- hybridsearch_be/src/main/java/com/demo/hybridsearch/search/Service/SearchService.java
+++ hybridsearch_be/src/main/java/com/demo/hybridsearch/search/Service/SearchService.java
@@ -1,12 +1,16 @@
 package com.demo.hybridsearch.search.Service;
 
-import com.demo.hybridsearch.search.DTO.UserQueryDto;
+import co.elastic.clients.elasticsearch.core.SearchTemplateResponse;
+import co.elastic.clients.elasticsearch.core.search.Hit;
+import co.elastic.clients.json.JsonData;
+import com.demo.hybridsearch.elasticsearch.service.ElasticsearchService;
+import com.demo.hybridsearch.search.DTO.*;
+
 import lombok.RequiredArgsConstructor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
 
 import java.util.*;
 
@@ -14,29 +18,75 @@ import java.util.*;
 @RequiredArgsConstructor
 @Service
 public class SearchService {
+    private final EmbeddingService embeddingService;
+    private final ElasticsearchService elasticsearchService;
 
-    /*** 공통 로직 ***/
+    public static String keywordTemplateId = "news_keyword_search";
+    public static String vectorTemplateId = "news_vector_search";
+    public static String index="naver_news_total";
+
+    // 전체 검색
     public ResponseEntity<?> allSearch(UserQueryDto userQueryDto) {
         if (userQueryDto.getQuery().isEmpty()) {
             return ResponseEntity.badRequest().body("검색어를 입력해주세요.");
         } else {
-            return null;
+            // 임베딩 서비스 호출
+            SearchDto embedded = embeddingService.getEmbeddingRequest(userQueryDto);
 
+            // 키워드, 벡터검색 수행
+            SearchResultDto keyword = keywordSearch(userQueryDto.getQuery());
+            SearchResultDto vector = vectorSearch(embedded.getVector());
+            // SearchResultDto hybrid = hybridSearch(embedded.getQuery(), embedded.getVector());
+
+            // 결과 조합
+            List<SearchResultDto> list= new ArrayList<>();
+            list.add(keyword);
+            list.add(vector);
+            // list.add(hybrid);
+
+            return ResponseEntity.ok(list);
         }
     }
 
-    /***키워드 검색***/
-    public void keywordSearch(String query) {
-
+    //키워드 검색
+    public SearchResultDto keywordSearch(String query) {
+        SearchTemplateResponse<NewsIndexDto> response =
+                elasticsearchService.getElasticData(keywordTemplateId, index,
+                        Map.of("query", JsonData.of(query)));
+        List<NewsReturnDto> resultList = parse(response);
+        return new SearchResultDto("keyword", response.took(),resultList);
     }
 
-    /*** 벡터 검색 ****/
-    public void vectorSearch(List<Float> vector) {
+    // 벡터 검색
+    public SearchResultDto vectorSearch(List<Float> vector) {
+        SearchTemplateResponse<NewsIndexDto> response =
+                elasticsearchService.getElasticData(vectorTemplateId, index,
+                        Map.of("vector", JsonData.of(vector.toString())));
+        List<NewsReturnDto> resultList = parse(response);
 
+        return new SearchResultDto("vector", response.took(),resultList);
     }
 
-    /*** 하이브리드 검색 ***/
-    public void hybridSearch(String query, List<Float> vector) {
+    // 공통 로직
+    public List<NewsReturnDto> parse(SearchTemplateResponse<NewsIndexDto> response){
+        List<Hit<NewsIndexDto>> hits= response.hits().hits();
+        List<NewsReturnDto> finalResults = new ArrayList<>();
+        for(Hit<NewsIndexDto> hit : hits){
+            NewsReturnDto newsReturnDto = new NewsReturnDto();
+            //ES 응답에서 가져오기
+            newsReturnDto.setId(hit.id());
+            newsReturnDto.setScore(hit.score());
+            newsReturnDto.setSource(hit.source());
+            newsReturnDto.setHighlight(hit.highlight());
+
+            finalResults.add(newsReturnDto);
+
+            log.info("id: {}",newsReturnDto.getId());
+            log.info("score: {}",newsReturnDto.getScore());
+            log.info("source: {}",newsReturnDto.getSource());
+            log.info("highlight: {}",newsReturnDto.getHighlight());
+        }
+        return finalResults;
     }
 
 }
