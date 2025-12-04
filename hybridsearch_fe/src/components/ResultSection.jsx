0b0530b7287a1ed069064914663af7197946b2fb
박건우 <parkwoo9901@gmail.com>
2025-12-04 18:01:15 +0900

    fix: 뉴스 데이터 변수 매핑

--- hybridsearch_fe/src/components/ResultSection.jsx
+++ hybridsearch_fe/src/components/ResultSection.jsx
@@ -4,35 +4,40 @@ export default function ResultSection({ title, data }) {
   return (
     <div
       className="
-        border dark:border-gray-500 shadow-sm
-        dark:bg-gray-700
+        border dark:border-gray-600 
+        dark:bg-gray-800
         flex flex-col
         h-full
         overflow-hidden
+        rounded-xl
       "
     >
-      {/* 타이틀 고정 영역 */}
-      <div className="p-4 sticky top-0 bg-white dark:bg-gray-700 z-10">
+      <div className="p-4 sticky top-0 bg-white dark:bg-gray-800 z-10">
         <h2 className="font-bold text-lg text-center">{title}</h2>
       </div>
-      <hr className=" border-gray-300 dark:border-gray-500" />
-      {/* 내용 스크롤 영역 */}
-      <div
-        className="
-          flex-1 overflow-y-auto p-4 
-          custom-scrollbar
-        "
-      >
+      <hr className=" border-gray-300 dark:border-gray-600" />
+
+      {/* <hr className="border-gray-300 dark:border-gray-500" /> */}
+
+      <div className="flex-1 overflow-y-auto p-4 custom-scrollbar">
         {data.length === 0 ? (
           <p className="opacity-60 text-center">결과 없음</p>
         ) : (
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col ">
             {data.slice(0, 5).map((item, idx) => (
-              <ResultCard
+              <div
                 key={idx}
-                question={item.question}
-                answer={item.answer}
-              />
+                className={`pb-2 ${
+                idx < 4 ? "border-b border-gray-300 dark:border-gray-600 mb-4" : ""
+                }`}
+              >
+                <ResultCard
+                  title={item.source?.title}
+                  description={item.source?.description}
+                  link={item.source?.link}
+                  highlight={item.highlight}
+                />
+              </div>
             ))}
           </div>
         )}
