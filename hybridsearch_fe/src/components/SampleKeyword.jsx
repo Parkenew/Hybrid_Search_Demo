f24f2f32a532a1345efdd8b5acf2fc4337ad584f
박건우 <parkwoo9901@gmail.com>
2025-12-18 22:31:43 +0900

    style: 샘플 키워드 스타일 수정
    리다이렉션 버튼 밑줄 -> 버튼으로 수정

--- hybridsearch_fe/src/components/SampleKeyword.jsx
+++ hybridsearch_fe/src/components/SampleKeyword.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 export default function SampleKeyword(){
     const navigate = useNavigate();
 
-    const examples = ["스타트업", "시작하는 기업"];
+    const examples = ["인공지능","서버 장애","애완동물","대기 오염을 줄이기 위한 시민들의 노력"];
 
     const handleExampleClick = (keyword) => {
     navigate(`/result?query=${encodeURIComponent(keyword)}`);
@@ -11,12 +11,17 @@ export default function SampleKeyword(){
 
     return (
         <div className="text-center font-bold">
-            <span className=" opacity-70">예시 검색어 : </span>
+            {/* <span className=" opacity-70">예시 검색어 : </span> */}
             {examples.map((ex, idx) => (
                 <button
                     key={idx}
                     onClick={() => handleExampleClick(ex)}
-                    className="mx-1 text-blue-600 dark:text-blue-300 underline hover:opacity-80"
+                    className="mx-1 text-gray-500 dark:text-gray-200
+                    border rounded-full dark:border-gray-600
+                    pt-2 pb-2 pl-3 pr-3 
+                    bg-[#f7f7f9] dark:bg-gray-900 
+                    text-sm hover:opacity-80"
+                    
                 >   
                     {ex}
                 </button>
