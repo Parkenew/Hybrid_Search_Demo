6bc3944c746c8c41acced1ef86e6d31526b5830b
박건우 <parkwoo9901@gmail.com>
2025-12-18 22:24:11 +0900

    feat: 메인페이지에서 네비바 로고 숨김 적용

--- hybridsearch_fe/src/components/NaviBar.jsx
+++ hybridsearch_fe/src/components/NaviBar.jsx
@@ -1,22 +1,29 @@
 import { useNavigate } from "react-router-dom";
 import ThemeToggle from "./ThemeToggle";
 
-export default function NaviBar(){
+export default function NaviBar({hideLogo=false}){
     const navigate = useNavigate();
     return (
-        <div className="shrink-0 w-full border-b border-gray-300 dark:border-gray-600 py-2 px-6 flex justify-between items-center">
-        <img
-          src="/lloydk_black.png"
-          alt="logo"
-          className="w-36 block dark:hidden cursor-pointer"
-          onClick={() => navigate("/")}
-        />
-        <img
-          src="/lloydk_white.png"
-          alt="logo"
-          className="w-36 hidden dark:block cursor-pointer"
-          onClick={() => navigate("/")}
-        />
+        <div className="shrink-0 w-full border-gray-300 dark:border-gray-600 py-2 px-6 flex justify-between items-center">
+        {hideLogo ? (
+          <div className="w-36"/>
+        ):(
+          <>
+            <img
+              src="/lloydk_black.png"
+              alt="logo"
+              className="w-36 block dark:hidden cursor-pointer"
+              onClick={() => navigate("/")}
+            />
+            <img
+              src="/lloydk_white.png"
+              alt="logo"
+              className="w-36 hidden dark:block cursor-pointer"
+              onClick={() => navigate("/")}
+            />
+          </>
+        )}
+        
         <ThemeToggle />
       </div>
     );
