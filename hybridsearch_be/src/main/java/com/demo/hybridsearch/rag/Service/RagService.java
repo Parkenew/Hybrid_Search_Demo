3066692edf1b6197f7a9e928b33d0a3488a01198
박건우 <parkwoo9901@gmail.com>
2025-12-24 09:04:08 +0900

    refactor: http request 함수 모듈화

--- hybridsearch_be/src/main/java/com/demo/hybridsearch/rag/Service/RagService.java
+++ hybridsearch_be/src/main/java/com/demo/hybridsearch/rag/Service/RagService.java
@@ -2,7 +2,10 @@ package com.demo.hybridsearch.rag.Service;
 
 import com.demo.hybridsearch.rag.DTO.RagRequestDTO;
 import com.demo.hybridsearch.search.DTO.NewsReturnDto;
+import com.demo.hybridsearch.search.DTO.SearchDto;
 import com.demo.hybridsearch.search.DTO.SearchResultDto;
+import com.demo.hybridsearch.search.DTO.UserQueryDto;
+import com.demo.hybridsearch.search.Service.SearchService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,7 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import org.springframework.web.reactive.function.client.WebClient;
-
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -24,31 +26,14 @@ public class RagService {
     private String llmHost;
 
     private final WebClient webClient = WebClient.builder().build();
+    private final SearchService searchService;
     private static final Pattern CONTENT_PATTERN = Pattern.compile("\"content\"\\s*:\\s*\"(.*?)\"", Pattern.DOTALL);
 
+
+    /************************************** 메인 함수 **************************************/
+    // 기존 RAG 함수: Stream 형식으로 응답 반환
     public Flux<String> getLLMStream(RagRequestDTO dto) {
-
-        String prompt = buildPrompt(dto);
-
-        Map<String, Object> requestBody = new HashMap<>();
-        requestBody.put("model", "openai/gpt-oss-120b");
-        requestBody.put("max_tokens", 10000);
-        requestBody.put("temperature", 0.3);
-        requestBody.put("include_reasoning", true);
-        requestBody.put("reasoning", Map.of("effort", "low"));
-        requestBody.put("stream", true);
-
-        List<Map<String, String>> messages = new ArrayList<>();
-        messages.add(Map.of("role", "system", "content", "당신은 RAG를 수행하는 어시스턴트입니다."));
-        messages.add(Map.of("role", "system", "content",
-                "사용자는 벡터 검색, 키워드 검색 결과와 검색어 그리고 요청사항을 제공합니다. " +
-                        "당신은 사용자가 준 문서를 근거로 요청사항을 해결해야 합니다. " +
-                        "키워드 검색과 벡터 검색의 차이도 분석하십시오. 표는 사용하지 마시오. 간략하게 대답하시오."));
-        messages.add(Map.of("role", "user", "content", prompt));
-
-        requestBody.put("messages", messages);
-
-        log.info("📝 LLM Stream Request Body: {}", requestBody);
+        Map<String, Object> requestBody = RAGPrompt(dto);
 
         return webClient.post()
                 .uri(llmHost)
@@ -62,9 +47,51 @@ public class RagService {
                 .map(c -> c);
     }
 
-    /**
-     * JSON Chunk에서 "content": "..." 값만 뽑아서 Flux<String> 으로 반환
-     */
+    // 리팩토링 중: 자연어로 질문 받았을때
+    public void getNaturalLanguage (UserQueryDto userQueryDto) {
+        Map<String, Object>  requestBody=queryToDSLPrompt(userQueryDto);
+        List<SearchResultDto> List=searchService.allSearch(userQueryDto);
+    }
+
+    /************************************** 프롬프트 세팅 **************************************/
+    // 사용자 요청을 queryDSL로 변환하는 프롬프트
+    private Map<String,Object> queryToDSLPrompt(UserQueryDto userQueryDto) {
+        String NL=userQueryDto.getQuery();
+        List<Map<String, String>> messages = new ArrayList<>();
+        messages.add(Map.of("role", "system", "content", "당신은 RAG를 수행하는 어시스턴트입니다."));
+        messages.add(Map.of("role", "system", "content","ES에 검색할 수 있게 사용자의 요청을 분석해서 자연어를 ES에 던질 문장으로 바꿔"));
+        messages.add(Map.of("role", "user", "content", NL));
+        return setRequestBody(messages,false);
+    }
+
+    // RAG를 위한 ES 검색 결과 전달 및 프롬프트
+    private Map<String, Object> RAGPrompt(RagRequestDTO dto) {
+        String prompt = buildPrompt(dto);
+        List<Map<String, String>> messages = new ArrayList<>();
+        messages.add(Map.of("role", "system", "content", "당신은 RAG를 수행하는 어시스턴트입니다."));
+        messages.add(Map.of("role", "system", "content",
+                "사용자는 벡터 검색, 키워드 검색 결과와 검색어 그리고 요청사항을 제공합니다. " +
+                        "당신은 사용자가 준 문서를 근거로 요청사항을 해결해야 합니다. " +
+                        "키워드 검색과 벡터 검색의 차이도 분석하십시오. 표는 사용하지 마시오. 간략하게 대답하시오."));
+        messages.add(Map.of("role", "user", "content", prompt));
+       return setRequestBody(messages,true);
+    }
+
+
+    /************************************** RequestBody 세팅 **************************************/
+    private Map<String,Object> setRequestBody(List<?> messages,boolean flag){
+        Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("model", "openai/gpt-oss-120b");
+        requestBody.put("max_tokens", 10000);
+        requestBody.put("temperature", 0.3);
+        requestBody.put("include_reasoning", true);
+        requestBody.put("reasoning", Map.of("effort", "low"));
+        requestBody.put("stream", flag);
+        requestBody.put("messages", messages);
+        return requestBody;
+    }
+
+    // GPT Stream 응답에서 content 값 추출
     private Flux<String> extractContent(String chunk) {
 
         Matcher matcher = CONTENT_PATTERN.matcher(chunk);
@@ -79,7 +106,7 @@ public class RagService {
 
         return Flux.fromIterable(list);
     }
-
+    // ES 결과 => String 으로 변환
     private String buildPrompt(RagRequestDTO dto) {
         StringBuilder sb = new StringBuilder();
 
